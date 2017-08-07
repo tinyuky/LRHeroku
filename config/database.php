9d5e2ca01a0059d0d5d104f80911c75e739903b8
NTT <tint139402@gmail.com>
2017-08-07 20:34:56 +0700

    fix db

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'sql12.freemysqlhosting.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'sql12188979'),
+            'username' => env('DB_USERNAME', 'sql12188979'),
+            'password' => env('DB_PASSWORD', 'VFipRMlQBr'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', 'sql12.freemysqlhosting.net'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'sql12188979'),
+            'username' => env('DB_USERNAME', 'sql12188979'),
+            'password' => env('DB_PASSWORD', 'VFipRMlQBr'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
