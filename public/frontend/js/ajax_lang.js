b7d7425cc0e7b26628fa657090049cf06fec0327
NTT <tint139402@gmail.com>
2017-08-08 10:25:54 +0700

    fix lang

--- public/frontend/js/ajax_lang.js
+++ public/frontend/js/ajax_lang.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('.multiln').click(function(){
     var _token = $('input[name="_token"]').val();
     $.ajax({
-      url: URL::route('shop.mullang'),
+      url: '{{URL::route('shop.mullang')}}',
       type: "post",
       data: { _token : _token,'locale':$(this).find('p').text()},
       datatype:'JSON',
